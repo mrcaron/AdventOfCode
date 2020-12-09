306f078992faadede04cfd0de053d31161666944
Michael Caron <mrcaron@users.noreply.github.com>
2020-12-09 10:23:00 -0600

    test input for AoC works

--- test/TestSumFinder.cpp
+++ test/TestSumFinder.cpp
@@ -20,3 +20,16 @@ TEST (SumFinder_Find2, TwoNumbers) {
     ASSERT_THAT(returned, A<vector<int>>());
     ASSERT_THAT(returned, UnorderedElementsAre(2000,20));
 }
+
+// These two numbers should add up to 2020
+TEST (SumFinder_Find2, FiveNumbers) {
+
+    auto sumFinderObj = SumFinder(2020);
+
+    vector<int> input{1721,979,366,299,675,1456}; 
+    auto returned = sumFinderObj.Find2(input);
+
+    ASSERT_THAT(returned.at(0), A<int>());
+    ASSERT_THAT(returned, A<vector<int>>());
+    ASSERT_THAT(returned, UnorderedElementsAre(1721,299));
+}
