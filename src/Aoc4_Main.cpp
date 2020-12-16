89dff34674ae9904ad967a29593d5df330e8494c
Michael Caron <mrcaron@users.noreply.github.com>
2020-12-16 08:44:58 -0600

    off-by-one error took me a whole day to figure out... :(

--- src/Aoc4_Main.cpp
+++ src/Aoc4_Main.cpp
@@ -1,11 +1,14 @@
+//#define _GLIBCXX_USE_CXX11_ABI 0
+
 #include <sys/stat.h>
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <memory>
-#include "PasswordVerifier.h"
-#include "TreeDetector.h"
+#include <iomanip>
+#include <algorithm>
+#include "PassportVerifier.h"
 
 using namespace AdventOfCode2020;
 
@@ -16,18 +19,25 @@ int main(int argc, char** argv)
 {
     // INPUT: --------------------------------------------------------------------------------------------------------------
     // INPUT: Check Input File Name
+    std::string filename;
     if (argc < 2)
     {
-        std::cerr << "Please provide a filename for input." << std::endl;
-        return 1;
+        //std::cerr << "Please provide a filename for input." << std::endl;
+        //return 1;
+        filename = "../../puzzle_input.txt";
+    } 
+    else {
+        filename = argv[1];
     }
-    std::string sPath{argv[1]};
+
+    std::string sPath{filename};
     struct stat buffer;
     if (stat (sPath.c_str(), &buffer) != 0)
     {
         std::cerr << "!! ERROR !! : " << sPath << " doesn't exist. Please provide an existing input file." << std::endl;
         return 1; 
     }
+    
     // END INPUT: ----------------------------------------------------------------------------------------------------------
 
     // PROCESS: ------------------------------------------------------------------------------------------------------------
@@ -38,6 +48,32 @@ int main(int argc, char** argv)
         std::cerr << "!! ERROR !! : opening " << sPath << " failed." << std::endl;
         return 1;
     }
+
+    int totalGood = 0;
+    int total = 0;
+    PassportVerifier verifier; 
+    std::stringstream input;
+    for(std::string buffer; std::getline(fsInput, buffer);)
+    {
+        if (buffer.empty())
+        {
+            total++;
+            bool good = verifier.Verify(input.str());
+            if (good)
+            {
+                totalGood++;
+            } 
+        
+            input.str("");
+            input.clear();
+        } else
+        {
+            input << buffer << " ";
+        }
+        
+    }
+
+    std::cout << "TOTAL GOOD PASSWORDS: " << totalGood << std::endl;
 
     return 0;
 }
--- src/PassportVerifier.cpp
+++ src/PassportVerifier.cpp
@@ -1,25 +1,37 @@
 #include <string>
 #include <sstream>
 #include <cstddef>
+#include <iostream>
 #include "PassportVerifier.h"
 
+// Some later TODO; I could use a BitSet to do this as well. 
 bool AdventOfCode2020::PassportVerifier::Verify( std::string input )
 {
     std::stringstream inputStream {input};
     std::byte passport {0};
 
+    //std::cout << "  >> DEBUG << Input string: " << input << std::endl;
+
     for(std::string token; std::getline(inputStream, token, ' ');)
     {
         int pos = token.find(':');
         auto key = token.substr(0,pos);
+        auto value = token.substr(pos+1);
+
         auto it = KEYS.find(key);
         if (it != KEYS.end())
         {
+            //std::cout << "  >> DEBUG << KEY: " << it->first << std::endl;
+            //std::cout << "  >> DEBUG << VALUE: " << static_cast<unsigned short>(it->second) << std::endl;
             passport |= it->second;
         }
     }
-
+    
+    //std::cout << "  >> DEBUG << PASSPORT VAL: " << static_cast<unsigned short>(passport) << std::endl;
     passport ^= std::byte{255};
 
-    return static_cast<unsigned char>(passport) < 2;
+    bool result =  static_cast<unsigned short>(passport) < 2;
+    
+    //std::cout << "  >> DEBUG << VALID?: " << (result ? "Y" : "N") << std::endl;
+    return result;
 }
